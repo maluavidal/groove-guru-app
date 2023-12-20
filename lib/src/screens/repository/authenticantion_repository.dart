144c1857b17c450de7b9cb47d4b1ce2a63f7abcf
emanuellfelipe <emanuelfelipe2004@gmail.com>
2023-12-20 19:52:20 -0300

    commit

---
+++ lib/src/screens/repository/authenticantion_repository.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:groove_guru_app/src/screens/home.dart';
+import 'package:groove_guru_app/src/screens/register.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  final _auth = FirebaseAuth.instance;
+  late final Rx<User?> firebaseUser;
+
+  @override
+  void onReady() {
+    firebaseUser = Rx<User?>(_auth.currentUser);
+    firebaseUser.bindStream(_auth.userChanges());
+    ever(firebaseUser, _setInitialScreen);
+  }
+
+  _setInitialScreen(User? user) {
+    user == null
+        ? Get.offAll(() => const Register())
+        : Get.offAll(() => const Home());
+  }
+
+  Future<void> createUserWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+    } catch (_) {}
+  }
+
+  Future<void> loginWithEmailAndPassword(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+    } catch (_) {}
+  }
+
+  Future<void> logout() async => await _auth.signOut();
+}
