bfa85c12f41f8c424efd400ac98c9c2dc1c3f4f8
marleypm16 <yarlempm16@gmail.com>
2024-02-12 00:48:35 -0300

    feat: routes

--- lib/src/routes.dart
+++ lib/src/routes.dart
@@ -3,17 +3,21 @@ import 'screens/home.dart';
 import 'screens/login.dart';
 import 'screens/register.dart';
 import 'screens/user_playlists.dart';
+import 'screens/music_select.dart'; // Importe a página music_select.dart aqui
 
 class Routes {
   static const String home = '/';
   static const String login = '/login';
   static const String register = '/register';
   static const String userPlaylists = '/user-playlists';
+  static const String musicSelect = '/music-select';
 
   static final Map<String, WidgetBuilder> routes = {
     home: (context) => const Home(),
     login: (context) => const Login(),
     register: (context) => Register(),
     userPlaylists: (context) => const UserPlaylists(),
+    musicSelect: (context) =>
+        const MusicListPage(), // Corrigido para MusicSelectPage
   };
 }
