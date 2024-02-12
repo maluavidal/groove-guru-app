1fd70040cd235b70200a568ab58248220da36248
Malu Vidal <106603917+maluavidal@users.noreply.github.com>
2024-02-12 01:10:27 -0300

    Update lib/src/screens/music_info.dart

--- lib/src/screens/music_info.dart
+++ lib/src/screens/music_info.dart
@@ -104,7 +104,7 @@ class MusicInfo extends StatelessWidget {
         text: TextSpan(
           style: TextStyle(
               color: Colors.black,
-              fontSize: 19.0), // Ajuste o tamanho da fonte aqui
+              fontSize: 19.0),
           children: [
             TextSpan(
               text: '$label: ',
